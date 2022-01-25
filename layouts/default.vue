d7eeb858a6fe9d0cac485c1d66a4440353c100b5
job-artem <job.artem.2019@gmail.com>
2022-01-25 14:10:10 +0200

    https://stepik.org/lesson/455333/step/1?unit=445750

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Navbar />
+    <Slider />
+    <div class="container">
+      <Nuxt />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+
+<script>
+import Navbar from "~/components/Navbar";
+import Footer from "../components/Footer";
+import Slider from "../components/Slider";
+
+export default {
+  components: {
+    Slider,
+    Footer,
+    Navbar,
+  }
+};
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Привет, это почти главная страница</h1>
+
+    <div v-for="post in posts" :key="post.slug" class="blog">
+      <div class="blog-title">{{ post.h1 }}</div>
+      <div class="blog-introtext">{{ post.description }}</div>
+      <div class="blog-tag">{{ post.tags }}</div>
+    </div>
+
+  </div>
+
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'IndexPage'
+  async asyncData(ctx) {
+    const { data } = await axios.get(`http://127.0.0.1:8000/api/posts/`);
+    return {
+      posts: data.results,
+    }
+  }
 }
 </script>
+
+<style>
+
+</style>
